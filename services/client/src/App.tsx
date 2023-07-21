0d81c0e8d38a0439d42989f6c2ff4fe62e7047ff
JhonSmith0 <zamuplays@gmail.com>
2023-07-20 22:57:37 -0300

    docker starter files

--- services/client/src/App.tsx
+++ services/client/src/App.tsx
@@ -1,8 +1,12 @@
+import axios from "axios";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { routes } from "./routes";
 
 const router = createBrowserRouter(routes);
+axios.get(import.meta.env.VITE_API_URL).then((e) => {
+  console.log(e.data);
+});
 function App() {
   return <RouterProvider router={router} />;
 }
--- services/client/vite.config.ts
+++ services/client/vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import vitePath from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react-swc";
+import { lookup } from "dns/promises";
+import { defineConfig } from "vite";
+import vitePath from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), vitePath()],
-})
+const envs = ["VITE_API_HOST", "VITE_API_PORT"];
+
+async function resolveApiIpByHost(host: string) {
+  return (await lookup(host)).address;
+}
+
+export default async () => {
+  for (const env_key of envs) {
+    const env_value = process.env[env_key];
+    if (!env_value) throw new Error(`Missing env property called "${env_key}"`);
+  }
+
+  const [HOST, PORT] = [process.env.VITE_API_HOST, process.env.VITE_API_PORT];
+
+  const apiIpAdress = await resolveApiIpByHost(HOST);
+  process.env.VITE_API_URL = `http://${apiIpAdress}:${PORT}`;
+
+  console.log(process.env.VITE_API_URL);
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react(), vitePath()],
+  });
+};
