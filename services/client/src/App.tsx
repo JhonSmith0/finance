a6de5f7f402519737b20520bcc322a7dbe39991a
JhonSmith0 <zamuplays@gmail.com>
2023-07-23 11:37:44 -0300

    removed invalid code

--- services/client/src/App.tsx
+++ services/client/src/App.tsx
@@ -1,12 +1,9 @@
-import axios from "axios";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { routes } from "./routes";
 
 const router = createBrowserRouter(routes);
-axios.get(import.meta.env.VITE_API_URL).then((e) => {
-  console.log(e.data);
-});
+
 function App() {
   return <RouterProvider router={router} />;
 }
