00fdd22eb9485cba789308b0fa03fa51c001e0f8
JhonSmith0 <zamuplays@gmail.com>
2023-07-29 18:07:23 -0300

    removed redundant line

--- services/api/src/services/TransactionService.ts
+++ services/api/src/services/TransactionService.ts
@@ -1,14 +1,13 @@
-import { Injectable, UseGuards } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { TransactionNewDTO } from 'src/dto/transaction/TransactionNewDTO';
-import { UserLogged } from 'src/guards/UserLogged';
 import { Transaction } from 'src/models/Transaction';
 import { User } from 'src/models/User';
 import { PrismaService } from './PrismaService';
 
-@UseGuards(UserLogged)
 @Injectable()
 export class TransactionService {
   constructor(private database: PrismaService) {}
+
   public async create(data: TransactionNewDTO, user: User) {
     const transaction = new Transaction({ ...data, userId: user.id });
 
