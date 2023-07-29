6b3d2743f8b46bfde391cafeebeb2bce2b24f66a
JhonSmith0 <zamuplays@gmail.com>
2023-07-29 18:45:18 -0300

    client: get all transactions service

--- services/client/src/services/transaction/index.ts
+++ services/client/src/services/transaction/index.ts
@@ -1,9 +1,13 @@
-import { ITransactionCreate } from "@/types";
+import { ITransaction, ITransactionCreate } from "@/types";
 import { api } from "../http";
 
 export class TransactionService {
   public static async create(data: ITransactionCreate) {
-    const result = await api.post("/transaction", data);
+    const result = await api.post<ITransaction>("/transaction", data);
+    return result.data;
+  }
+  public static async getAll() {
+    const result = await api.get<ITransaction[]>("/transaction");
     return result.data;
   }
 }
